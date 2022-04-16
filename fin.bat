045f182a3eb553f0ab62ea8b42c817a4f86f07b2
TanyaDasha <politova-01@inbox.ru>
2022-04-16 16:33:58 +0300

    Laba2

---
+++ fin.bat
@@ -0,0 +1,29 @@
+echo off
+rem create new catalog
+md politovashirokova
+rem go to into new catalog
+cd politovashirokova
+md tanyadasha
+md adb1908
+rem this need for pause of programm
+pause
+echo off
+rem create 1306200124072000
+echo > 1306200124072000.txt
+rem go to into adb1908
+cd adb1908
+rem create 003
+echo > 003.txt
+cd..
+cd..
+pause
+echo off
+del politovashirokova /S /Q /F
+pause
+echo off
+cd politovashirokova
+rd tanyadasha
+rd adb1908
+cd..
+rd politovashirokova
+pause
